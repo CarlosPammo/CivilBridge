3d4dc21a3b216f862d776a98eb2a8f656af3ae93
Carlos Pammo <Carlos.Pammo@outlook.com>
2017-02-17 17:45:40 -0400

    Submitting missing menu files

---
+++ Concreter/js/common/menu.js
@@ -0,0 +1,14 @@
+﻿angular.module("menu", [])
+.directive("topMenu", [
+	function() {
+		return {
+			retrict: "E",
+			replace: true,
+			scope: false,
+			link: function(scope, element, attrs) {
+				
+			},
+			templateUrl: "js/common/menu.html"
+		};
+	}
+])
